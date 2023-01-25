7aad43419810650391fcc8cb8d7580018f6d654b
Gangxin-Li <lgx.uofg@gmail.com>
2023-01-25 21:45:11 +0000

    M4-T2

--- manual_rps.py
+++ manual_rps.py
@@ -8,9 +8,9 @@ def get_user_choice():
 def get_winner(computer_choice, user_choice):
     computer=computer_choice
     user = user_choice
-    if computer == 'Rock' and user == 'Scissors' or computer =='paper' and user=='Rock' or computer=='Scissors' and user=='Paper':
+    if computer == 'Rock' and user == 'Scissors' or computer =='Paper' and user=='Rock' or computer=='Scissors' and user=='Paper':
         print("You lost")
-    elif user == 'Rock' and computer == 'Scissors' or user =='paper' and computer=='Rock' or user=='Scissors' and computer=='Paper':
+    elif user == 'Rock' and computer == 'Scissors' or user =='Paper' and computer=='Rock' or user=='Scissors' and computer=='Paper':
         print("You won!")
     else:
         print("It is a tie!")
