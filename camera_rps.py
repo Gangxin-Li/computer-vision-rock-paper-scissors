bc947767a71a1e7edf8457e12035e6d98012fe0a
Gangxin-Li <lgx.uofg@gmail.com>
2023-01-25 22:03:52 +0000

    M5-T2

--- camera_rps.py
+++ camera_rps.py
@@ -1,11 +1,12 @@
 import cv2
 from keras.models import load_model
 import numpy as np
+import time
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-
+start = time.time()
 def get_prediction():
     ret, frame = cap.read()
     resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
@@ -16,3 +17,5 @@ def get_prediction():
     cv2.imshow('frame', frame)
     # Press q to close the window
     print(prediction)
+end = time.time()
+print(end-start,'s')
