e39ebb7164eff3ec52b31696e13eae0836fa6730
Gangxin-Li <lgx.uofg@gmail.com>
2023-01-25 21:41:44 +0000

    M4-T2

--- manual_rps.py
+++ manual_rps.py
@@ -5,4 +5,12 @@ def get_computer_choice():
 def get_user_choice():
     choice = input("give your choice:")
     return choice
-    
+def get_winner():
+    computer = get_computer_choice()
+    user = get_computer_choice()
+    if computer == 'Rock' and user == 'Scissors' or computer =='paper' and user=='Rock' or computer=='Scissors' and user=='Paper':
+        print("You lost")
+    elif user == 'Rock' and computer == 'Scissors' or user =='paper' and computer=='Rock' or user=='Scissors' and computer=='Paper':
+        print("You won!")
+    else:
+        print("It is a tie!")
